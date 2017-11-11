e72dcaff62f64273e876e782423214c80e5fb4ce
carlosgonzalez96 <carlosgonz817@gmail.com>
2017-11-11 12:23:10 -0800

    hw 00
    first home work assigment

---
+++ hw00/01-hello world.py
@@ -0,0 +1,11 @@
+################################
+# CS 261 - Fall 2017
+# Homework 0
+# Name: Carlos Gonzalez
+################################
+
+def hello_world(val=''):
+    print("Hello, World!")
+    print val;
+    return 0;
+hello_world("BYE")
